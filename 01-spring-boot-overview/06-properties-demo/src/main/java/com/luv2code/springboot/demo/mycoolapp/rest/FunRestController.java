a6134a83ff64fb4d9947b6408224c7826a75b08a
David Espinosa <david.espinosa.21@gmail.com>
2025-07-12 01:04:17 -0500

    Finish properties demo

--- 01-spring-boot-overview/06-properties-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 01-spring-boot-overview/06-properties-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,24 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // Inject properties for: coach.name and team.name from application.properties
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // Expose new endpoint for "teaminfo"
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return String.format("Coach: %s, Team name: %s", coachName, teamName);
+    }
 
     // Expose "/" that returns "Hello World"
     // This should cause localhost:8080 to show "Hello World" when the application is run
@@ -24,4 +38,5 @@ public class FunRestController {
     public String ping() {
         return "pong!";
     }
+
 }
