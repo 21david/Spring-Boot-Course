45915c91ffc83921632bb92ee2cd62b62521f09f
David Espinosa <david.espinosa.21@gmail.com>
2025-07-12 18:33:52 -0500

    Finish setter injection section

--- 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -11,13 +11,16 @@ public class DemoController {
     // Define a private field for the dependency
     private Coach myCoach;
 
-    // Define a constructor for dependency injection (constructor injection - it injects the coach)
-    @Autowired  // Optional since there's only 1 constructor, but left for academic purposes
-    public DemoController(Coach theCoach) {
-        // Spring figures out that this is the CricketCoach since it's the only one.
-        // Otherwise we would need configuration I believe
+    @Autowired
+    public void setCoach(Coach theCoach) {
         myCoach = theCoach;
     }
+
+    /* // Autowired here will guarantee that Spring will inject a coach
+    @Autowired
+    public void anyMethodNameHere(Coach theCoach) {
+        myCoach = theCoach;
+    }*/
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
