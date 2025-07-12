7db7509caff51ace17a79d90a2e1a7045ed4ff59
David Espinosa <david.espinosa.21@gmail.com>
2025-07-12 18:34:20 -0500

    Start qualifiers section

---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,11 @@
+package com.luv2code.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component  // Marks this class as a Spring Bean, and makes available for Dependency Injection
+public class CricketCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes! asdf :)";
+    }
+}
