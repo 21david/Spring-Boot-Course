02f3d5f405384181288349508111580d7e8d3cdb
David Espinosa <david.espinosa.21@gmail.com>
2025-07-12 19:57:37 -0500

    Start lazy initialization

---
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
@@ -0,0 +1,17 @@
+package com.luv2code.springcoredemo.common;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Primary // mark as primary so that @Autowired picks this one by default when there are multiple
+@Component
+public class TennisCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        // IntelliJ shortcut - 'sout', 'soutv', 'soutm', or 'serr' then press tab for System.out.println(...);
+        System.out.println();
+        return "Practice your backhand volley. 🎾🏸";
+    }
+
+}
