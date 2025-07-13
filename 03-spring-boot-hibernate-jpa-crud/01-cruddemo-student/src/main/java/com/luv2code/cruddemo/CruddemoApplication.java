836f624f22266b3cd012490f09e301ce9508d37f
David Espinosa <david.espinosa.21@gmail.com>
2025-07-13 18:10:47 -0500

    Add entity class and commandLineRunner method

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,7 +1,9 @@
 package com.luv2code.cruddemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CruddemoApplication {
@@ -10,4 +12,12 @@ public class CruddemoApplication {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
 
+	@Bean
+	// This will execute after spring beans are loaded
+	public CommandLineRunner commandLineRunner(String[] args) {
+		System.out.println("CruddemoApplication.commandLineRunner");
+		return runner -> {
+			System.out.println("Hello World!");
+		};
+	}
 }
