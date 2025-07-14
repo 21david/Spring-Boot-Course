b7414a084e3439dd3b94ad54c3b9150af667b80a
David Espinosa <david.espinosa.21@gmail.com>
2025-07-14 13:34:09 -0500

    Finish section 3 (add database-related configuration for logging and auto dropping/creating/keeping tables)

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -28,7 +28,7 @@ public class CruddemoApplication {
 
 //			createStudent(studentDAO);
 
-//			createMultipleStudents(studentDAO);
+			createMultipleStudents(studentDAO);
 
 //			readStudent(studentDAO);
 
@@ -40,7 +40,7 @@ public class CruddemoApplication {
 
 //			deleteStudent(studentDAO);
 
-			deleteAllStudents(studentDAO);
+//			deleteAllStudents(studentDAO);
 		};
 
 	}
