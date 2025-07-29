01d532009e2ac032436d99596408ed3a9c48497e
David Espinosa <david.espinosa.21@gmail.com>
2025-07-28 23:23:58 -0500

    Halfway through full database + rest controller employee project

---
+++ 04-spring-boot-rest-crud/Playground/src/Main.java
@@ -0,0 +1,16 @@
+
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args)  {
+
+
+    }
+
+    static void takeDouble(double in) {
+        Object obj = "Hello";
+        Integer x = (Integer) obj;  // ❌ String cannot be cast to Integer
+    }
+}
