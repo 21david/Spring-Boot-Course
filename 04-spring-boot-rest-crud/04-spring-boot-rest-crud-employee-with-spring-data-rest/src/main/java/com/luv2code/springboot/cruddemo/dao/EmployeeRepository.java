6d65f00a801c0c6446db8d57926c111e7adb00a7
David Espinosa <david.espinosa.21@gmail.com>
2025-07-31 22:55:33 -0500

    Try different REST API configurations

--- 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,9 +2,11 @@ package com.luv2code.springboot.cruddemo.dao;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 // Employee = entity or database table, Integer = primary key (int id)
 // This is the easier alternative to our previous DAO interface and implementation
+// @RepositoryRestResource(path="members")  // expose at endpoint /magic-api/members instead of /magic-api/employees
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // All the methods, findAll(), save(), deleteById(), etc, are automatically given to us
     // just by extending JpaRepository!
