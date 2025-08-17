94c7cbf046ef736d6c3c3d513c78504695cb98e9
David Espinosa <david.espinosa.21@gmail.com>
2025-08-16 19:54:35 -0500

    Add manager access to patch requests

---
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -0,0 +1,86 @@
+package com.luv2code.springboot.cruddemo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    // Create users
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails john = User.builder()
+                .username("eric")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails david = User.builder()
+                .username("david")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(david, mary, john);
+    }
+
+    // Restrict access based on roles
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        // Read access
+                        .requestMatchers(
+                                HttpMethod.GET,
+                                "/api/employees",
+                                "/api/employees/**"  // ** = match all subpaths, in this case an id
+                        ).hasRole("EMPLOYEE")  // give only Employees access to GET methods at these paths
+
+                        // Create access
+                        .requestMatchers(
+                                HttpMethod.POST,
+                                "/api/employees"
+                        ).hasRole("MANAGER")
+
+                        // Update access
+                        .requestMatchers(
+                                HttpMethod.PUT,
+                                "/api/employees"
+                        ).hasRole("MANAGER")
+
+                        // Update access (patch)
+                        .requestMatchers(
+                                HttpMethod.PATCH,
+                                "/api/employees/**"
+                        ).hasRole("MANAGER")
+
+                        // Delete access
+                        .requestMatchers(
+                                HttpMethod.DELETE,
+                                "/api/employees/**"
+                        ).hasRole("ADMIN")
+        );
+
+        // use HTTP Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // Disable CSRF, not generally required for stateless REST APIS that use POST, PUT, DELETE and/or PATCH
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
