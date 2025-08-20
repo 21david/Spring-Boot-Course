217fd5006bca74b4eefa7a3f5ab302548c970a14
David Espinosa <david.espinosa.21@gmail.com>
2025-08-19 20:01:29 -0500

    Add student form with hardcoded countries

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/udemycourse/springboot/thymeleafdemo/controller/StudentController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/udemycourse/springboot/thymeleafdemo/controller/StudentController.java
@@ -2,28 +2,31 @@ package com.udemycourse.springboot.thymeleafdemo.controller;
 
 import com.udemycourse.springboot.thymeleafdemo.model.Student;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
-
+    
     @GetMapping("/showStudentForm")
     public String showForm(Model model) {
 
         model.addAttribute("student", new Student());
 
-        return "studentform";
+        return "student-form";
     }
 
-    @PostMapping("/")
+    @PostMapping("/processStudentForm")
     public String processStudent(@ModelAttribute("student") Student student) {
 
+        System.out.println("The student's fields have been set to: " + student.getFirstName() + " " + student.getLastName() + " through the form.");
 
-
-        return "studentconfirmation";
+        return "student-confirmation";
     }
 }
