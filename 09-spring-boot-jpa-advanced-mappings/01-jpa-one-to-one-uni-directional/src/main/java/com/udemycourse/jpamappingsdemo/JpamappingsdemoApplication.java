684eceac01ec20d6bc8992fea3d81a25e024a27a
David Espinosa <david.espinosa.21@gmail.com>
2025-08-22 11:05:58 -0500

    Start section 9 / Add fresh project for JPA mappings

---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni-directional/src/main/java/com/udemycourse/jpamappingsdemo/JpamappingsdemoApplication.java
@@ -0,0 +1,23 @@
+package com.udemycourse.jpamappingsdemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class JpamappingsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpamappingsdemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+
+		return runner -> {
+			System.out.println("Hello world");
+		};
+	}
+
+}
---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni-directional/src/test/java/com/udemycourse/jpamappingsdemo/JpamappingsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemycourse.jpamappingsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpamappingsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
