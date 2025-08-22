1d3d5f12bf3a3cc2194ce19eb3f992d1791e98e7
David Espinosa <david.espinosa.21@gmail.com>
2025-08-21 22:57:11 -0500

    Finish section 8
    Skipped last few videos as they were a repeat of previous security concepts (custom tables, bcrypt password encryption)
    Video 284

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/udemycourse/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/udemycourse/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -4,36 +4,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails ember = User.builder()
-                .username("ember")
-                .password("{noop}pass")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}pass")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-        UserDetails adam = User.builder()
-                .username("adam")
-                .password("{noop}pass")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(ember, mary, adam);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        // SB will get user info from the 'users' and 'authorities' tables in the database
+        // all automatically
+        return new JdbcUserDetailsManager(dataSource);
     }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -59,4 +48,29 @@ public class DemoSecurityConfig {
         return http.build();
     }
 
+
+    /*
+        @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails ember = User.builder()
+                .username("ember")
+                .password("{noop}pass")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}pass")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails adam = User.builder()
+                .username("adam")
+                .password("{noop}pass")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(ember, mary, adam);
+    }
+     */
 }
