b2bf4135138592970446d77383e870385520a722
David Espinosa <david.espinosa.21@gmail.com>
2025-08-24 12:32:01 -0500

    Start new section with (many to many) (course <-> student). Duplicate previous project folder.

---
+++ 09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/udemycourse/jpamappingsdemo/dao/AppDAO.java
@@ -0,0 +1,37 @@
+package com.udemycourse.jpamappingsdemo.dao;
+
+import com.udemycourse.jpamappingsdemo.entity.Course;
+import com.udemycourse.jpamappingsdemo.entity.Instructor;
+import com.udemycourse.jpamappingsdemo.entity.InstructorDetail;
+
+import java.util.List;
+
+public interface AppDAO {
+    void save(Instructor instructor);
+
+    Instructor findInstructorById(int id);
+
+    void deleteInstructorById(int id);
+
+    InstructorDetail findInstructorDetailById(int id);
+
+    void deleteInstructorDetailById(int id);
+
+    List<Course> findCoursesByInstructorId(int id);
+
+    Course findCourseById(int id);
+
+    Instructor findInstructorByIdJoinFetch(int id);
+
+    void update(Instructor instructor);
+
+    void update(Course course);
+
+    void deleteCourseById(int id);
+
+    void save(Course course);
+
+    Course findCourseAndReviewsByCourseId(int id);
+
+
+}
---
+++ 09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/udemycourse/jpamappingsdemo/entity/Course.java
@@ -0,0 +1,82 @@
+package com.udemycourse.jpamappingsdemo.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="course")
+public class Course {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="title")
+    private String title;
+
+    // Many courses can map to one instructor. We make sure deletions don't cascade.
+    @ManyToOne(cascade={CascadeType.DETACH, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinColumn(name="instructor_id")
+    private Instructor instructor;
+
+    @OneToMany(fetch = FetchType.LAZY,
+                cascade = CascadeType.ALL)  //CascadeType.ALL means anything done to this course will also happen to the reviews
+    @JoinColumn(name="course_id")  // the 'course_id' column in reviews is the one that points to this course
+    private List<Review> reviews;
+
+    public Course() {
+    }
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    public void add(Review review) {
+        if (reviews == null)
+            reviews = new ArrayList<>();
+
+        reviews.add(review);
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "title='" + title + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
