158cad262d413c4919a18c7a4dc07303f98deaa6
David Espinosa <david.espinosa.21@gmail.com>
2025-08-25 12:03:41 -0500

    Print method signature with JoinPoint

--- 10-spring-boot-aop/02-spring-boot-aop-pointcut-declarations/src/main/java/com/udemycourse/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-pointcut-declarations/src/main/java/com/udemycourse/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,8 +1,10 @@
 package com.udemycourse.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -11,12 +13,16 @@ import org.springframework.stereotype.Component;
 @Order(2)
 public class MyDemoLoggingAspect {
 
-    // This uses the pointcut declaration declared above that excludes getters and setters
     // Tip: right click UtilityExpressions file in IntelliJ -> Copy Path/Reference... -> Copy Reference (at the bottom)
     // to get the full name that is needed here: com.udemycourse.aopdemo.aspect.UtilityExpressions
     @Before("com.udemycourse.aopdemo.aspect.UtilityExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
+    public void beforeAddAccountAdvice(JoinPoint jp) {
         System.out.println("(2) ======>>> Executing @Before advice on addAccount()");
+
+        // Print method signature
+        MethodSignature signature = (MethodSignature) jp.getSignature();
+        System.out.println("Method: " + signature);
+
     }
 
 }
